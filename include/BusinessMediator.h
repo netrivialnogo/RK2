43324c7e95033b824369f9b7d5a7392df77e600b
netrivialnogo <kbelovwowcirkle@gmail.com>
2025-06-07 00:49:57 +0300

    Update BusinessMediator.h

--- include/BusinessMediator.h
+++ include/BusinessMediator.h
@@ -1,7 +1,7 @@
 #ifndef BUSINESSMEDIATOR_H
 #define BUSINESSMEDIATOR_H
 
-#include <cstdint>  
+#include <cstdint>
 
 class EstateOwner;
 class GroceryStore;
@@ -9,11 +9,18 @@ class Restaurant;
 
 class BusinessMediator {
 public:
+    BusinessMediator(EstateOwner& estateOwner, GroceryStore& groceryStore, Restaurant& restaurant);
     virtual ~BusinessMediator() = default;
-    virtual void EstateRentPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) = 0;
-    virtual void GroceryStockChanged(std::int32_t currentStock) = 0;
-    virtual void GroceryPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) = 0;
-    virtual void FoodIsCooked() = 0;
+    
+    virtual void EstateRentPriceChanged(std::int32_t oldPrice, std::int32_t newPrice);
+    virtual void GroceryStockChanged(std::int32_t currentStock);
+    virtual void GroceryPriceChanged(std::int32_t oldPrice, std::int32_t newPrice);
+    virtual void FoodIsCooked();
+
+protected:
+    EstateOwner& estateOwner_;
+    GroceryStore& groceryStore_;
+    Restaurant& restaurant_;
 };
 
-#endif 
+#endif
