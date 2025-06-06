5bd5a55d4f387dc5c080300cdf8c6a4efac3f944
netrivialnogo <kbelovwowcirkle@gmail.com>
2025-06-07 00:50:30 +0300

    Update BusinessMediator.cpp

--- src/BusinessMediator.cpp
+++ src/BusinessMediator.cpp
@@ -3,39 +3,30 @@
 #include "GroceryStore.h"
 #include "Restaurant.h"
 
+BusinessMediator::BusinessMediator(EstateOwner& estateOwner, 
+                                  GroceryStore& groceryStore, 
+                                  Restaurant& restaurant)
+    : estateOwner_(estateOwner),
+      groceryStore_(groceryStore),
+      restaurant_(restaurant) {}
 
-BusinessMediator::BusinessMediator(EstateOwner& estateOwner, GroceryStore& groceryStore, Restaurant& restaurant)
-    : estateOwner_(estateOwner), groceryStore_(groceryStore), restaurant_(restaurant)
-{
-    estateOwner_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
-    groceryStore_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
-    restaurant_.SetBusinessMediator(design::AccessKey<BusinessMediator>(), this);
-}
-
-void BusinessMediator::EstateRentPriceChanged(std::int32_t oldPrice, std::int32_t newPrice)
-{
+void BusinessMediator::EstateRentPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) {
     groceryStore_.AlterPrice((newPrice - oldPrice) / 10000);
     restaurant_.AlterPrice((newPrice - oldPrice) / 1000);
 }
 
-void BusinessMediator::GroceryStockChanged(std::int32_t currentStock)
-{
-    if (currentStock > 0)
-    {
+void BusinessMediator::GroceryStockChanged(std::int32_t currentStock) {
+    if (currentStock > 0) {
         restaurant_.SetIsOpened(design::AccessKey<BusinessMediator>(), true);
-    }
-    else
-    {
+    } else {
         restaurant_.SetIsOpened(design::AccessKey<BusinessMediator>(), false);
     }
 }
 
-void BusinessMediator::GroceryPriceChanged(std::int32_t oldPrice, std::int32_t newPrice)
-{
+void BusinessMediator::GroceryPriceChanged(std::int32_t oldPrice, std::int32_t newPrice) {
     restaurant_.AlterPrice(newPrice - oldPrice);
 }
 
-void BusinessMediator::FoodIsCooked()
-{
+void BusinessMediator::FoodIsCooked() {
     groceryStore_.Sell();
 }
