f516f3516a5a40b4654676315e68b8adfbaca082
netrivialnogo <kbelovwowcirkle@gmail.com>
2025-06-07 00:58:09 +0300

    Update test_BusinessMediator.cpp

--- test/test_BusinessMediator.cpp
+++ test/test_BusinessMediator.cpp
@@ -5,11 +5,13 @@
 #include "../include/GroceryStore.h"
 #include "../include/Restaurant.h"
 
-using ::testing::_;
+using namespace testing;
 
 class MockEstateOwner : public EstateOwner {
 public:
     MOCK_METHOD(std::int32_t, SetEstateRentPrice, (std::int32_t), (override));
+    MOCK_METHOD(BusinessMediator*, SetBusinessMediator, 
+               (design::AccessKey<BusinessMediator>, BusinessMediator*), (override));
 };
 
 class MockGroceryStore : public GroceryStore {
@@ -17,6 +19,8 @@ public:
     MOCK_METHOD(std::int32_t, Supply, (std::uint16_t), (override));
     MOCK_METHOD(std::int32_t, Sell, (), (override));
     MOCK_METHOD(std::int32_t, AlterPrice, (std::int32_t), (override));
+    MOCK_METHOD(BusinessMediator*, SetBusinessMediator,
+               (design::AccessKey<BusinessMediator>, BusinessMediator*), (override));
 };
 
 class MockRestaurant : public Restaurant {
@@ -24,6 +28,8 @@ public:
     MOCK_METHOD(std::int32_t, CookFood, (), (override));
     MOCK_METHOD(std::int32_t, AlterPrice, (std::int32_t), (override));
     MOCK_METHOD(void, SetIsOpened, (design::AccessKey<BusinessMediator>, bool), (override));
+    MOCK_METHOD(BusinessMediator*, SetBusinessMediator,
+               (design::AccessKey<BusinessMediator>, BusinessMediator*), (override));
 };
 
 TEST(BusinessMediatorTest, EstateRentPriceChanged) {
@@ -31,9 +37,11 @@ TEST(BusinessMediatorTest, EstateRentPriceChanged) {
     MockGroceryStore grocery;
     MockRestaurant restaurant;
     BusinessMediator mediator(owner, grocery, restaurant);
-    
+
     EXPECT_CALL(grocery, AlterPrice(0)).WillOnce(Return(100));
     EXPECT_CALL(restaurant, AlterPrice(0)).WillOnce(Return(500));
+    
+
     mediator.EstateRentPriceChanged(10000, 10000);
 }
 
@@ -42,11 +50,29 @@ TEST(BusinessMediatorTest, GroceryStockChanged) {
     MockGroceryStore grocery;
     MockRestaurant restaurant;
     BusinessMediator mediator(owner, grocery, restaurant);
-    
     auto testKey = design::AccessKey<BusinessMediator>::createForTesting();
     EXPECT_CALL(restaurant, SetIsOpened(testKey, true)).Times(1);
     mediator.GroceryStockChanged(5);
-    
     EXPECT_CALL(restaurant, SetIsOpened(testKey, false)).Times(1);
     mediator.GroceryStockChanged(0);
+}
+
+TEST(BusinessMediatorTest, GroceryPriceChanged) {
+    MockEstateOwner owner;
+    MockGroceryStore grocery;
+    MockRestaurant restaurant;
+    BusinessMediator mediator(owner, grocery, restaurant);
+
+    EXPECT_CALL(restaurant, AlterPrice(50)).WillOnce(Return(550));
+    mediator.GroceryPriceChanged(100, 150);
+}
+
+TEST(BusinessMediatorTest, FoodIsCooked) {
+    MockEstateOwner owner;
+    MockGroceryStore grocery;
+    MockRestaurant restaurant;
+    BusinessMediator mediator(owner, grocery, restaurant);
+
+    EXPECT_CALL(grocery, Sell()).WillOnce(Return(100));
+    mediator.FoodIsCooked();
 }
