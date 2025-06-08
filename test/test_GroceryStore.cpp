ed51de048ee3e06b4dbeed7a6e79c818148882b3
netrivialnogo <kbelovwowcirkle@gmail.com>
2025-06-08 14:37:59 +0300

    Update test_GroceryStore.cpp

--- test/test_GroceryStore.cpp
+++ test/test_GroceryStore.cpp
@@ -1,18 +1,38 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include "../include/GroceryStore.h"
+#include "../include/EstateOwner.h"    
+#include "../include/Restaurant.h"     
 #include "MockBusinessMediator.h"
 
 TEST(GroceryStoreTest, Supply_IncreasesStockAndNotifies) {
     GroceryStore store;
     EstateOwner dummyOwner;
-    GroceryStore dummyStore;
     Restaurant dummyRestaurant;
-    MockBusinessMediator mediator(dummyOwner, dummyStore, dummyRestaurant);
+    MockBusinessMediator mediator(dummyOwner, store, dummyRestaurant);
     
     store.SetBusinessMediator(design::AccessKey<BusinessMediator>::createForTesting(), &mediator);
 
     EXPECT_CALL(mediator, GroceryStockChanged(10)).Times(1);
     auto newStock = store.Supply(10);
     EXPECT_EQ(newStock, 10);
+}
+
+TEST(GroceryStoreTest, Sell_DecreasesStockAndNotifies) {
+    GroceryStore store;
+    EstateOwner dummyOwner;
+    Restaurant dummyRestaurant;
+    store.Supply(5); 
+    
+    MockBusinessMediator mediator(dummyOwner, store, dummyRestaurant);
+    store.SetBusinessMediator(design::AccessKey<BusinessMediator>::createForTesting(), &mediator);
+
+    EXPECT_CALL(mediator, GroceryStockChanged(4)).Times(1);
+    auto price = store.Sell();
+    EXPECT_GT(price, 0);
+}
+
+TEST(GroceryStoreTest, Sell_ThrowsWhenEmpty) {
+    GroceryStore store;
+    EXPECT_THROW(store.Sell(), std::logic_error);
 }
