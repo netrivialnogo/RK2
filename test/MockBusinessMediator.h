d6265097bc283b9145048e0d9158d5588b066cb2
netrivialnogo <kbelovwowcirkle@gmail.com>
2025-06-08 14:57:39 +0300

    Update MockBusinessMediator.h

--- test/MockBusinessMediator.h
+++ test/MockBusinessMediator.h
@@ -3,13 +3,21 @@
 
 #include <gmock/gmock.h>
 #include "../include/BusinessMediator.h"
+#include "../include/EstateOwner.h"
+#include "../include/GroceryStore.h"
+#include "../include/Restaurant.h"
 
 class MockBusinessMediator : public BusinessMediator {
 public:
+    MockBusinessMediator(EstateOwner& estateOwner, 
+                        GroceryStore& groceryStore, 
+                        Restaurant& restaurant)
+        : BusinessMediator(estateOwner, groceryStore, restaurant) {}
+    
     MOCK_METHOD(void, EstateRentPriceChanged, (std::int32_t, std::int32_t), (override));
     MOCK_METHOD(void, GroceryStockChanged, (std::int32_t), (override));
     MOCK_METHOD(void, GroceryPriceChanged, (std::int32_t, std::int32_t), (override));
     MOCK_METHOD(void, FoodIsCooked, (), (override));
 };
 
-#endif 
+#endif
